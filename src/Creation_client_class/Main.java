6f7219c826f68580c5ef08934db94b4afa11e708
Mehdi Fouad <mehdi.fouad@capgemini.com>
2022-01-13 15:38:57 +0100

    modification code

--- src/Creation_client_class/Main.java
+++ src/Creation_client_class/Main.java
@@ -1,10 +1,24 @@
 package Creation_client_class;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         //Array
 
+        test_set(5);
+    }
+
+    private static void test_set(int j) {
+        for (int i = 0; i < j; ++i) {
+            Client[] clients = new Client[]{new Client("name" + i, "firstname" + i, 47 + i)};
+            System.out.println(Arrays.toString(clients));
+        }
+
 
     }
+
 }
+
+
